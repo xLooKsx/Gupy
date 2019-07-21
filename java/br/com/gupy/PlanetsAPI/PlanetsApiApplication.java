5f3805f6709f543d9b7516e77a9f5beb26c5fe75
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2019-07-20 22:03:53 -0300

    commit inicial

---
+++ java/br/com/gupy/PlanetsAPI/PlanetsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gupy.PlanetsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsApiApplication.class, args);
+	}
+
+}
